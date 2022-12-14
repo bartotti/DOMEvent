e77b2fc0bec497850cc3a63d09f18a0e6b0b57fd
Your Name <real@email.com>
2022-12-14 15:34:51 -0500

    did exercise 2

--- index.js
+++ index.js
@@ -8,3 +8,21 @@ cookie_btn.addEventListener('click', function (){
     }
     display_count.innerHTML = count;
 });
+
+
+let change_small = document.getElementById ("smaller")
+let change_big = document.getElementById("bigger")
+let change_text = document.getElementById ("text")
+change_small.addEventListener('click', function (){
+    change_small.onclick = function(){
+change_text.style.fontSize="smaller"
+        console.log(change_small);
+    }
+})
+
+change_big.addEventListener('click', function (){
+    change_big.onclick = function(){
+change_text.style.fontSize="larger"
+        console.log(change_big);
+    }
+})
